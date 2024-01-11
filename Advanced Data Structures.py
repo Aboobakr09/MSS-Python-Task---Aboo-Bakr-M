f036bfd803462960b3e1526d9ce75be31bd9d18a
Aboo Bakr Muhammad <150986293+Aboobakr09@users.noreply.github.com>
2024-01-12 03:11:38 +0530

    Add files via upload

---
+++ Advanced Data Structures.py
@@ -0,0 +1,8 @@
+customer_info = {'name': 'Vinsmoke Sanji', 'age': 21, 'purchase history': ['Meat', 'Sake', 'Cotton-Candy']}
+customer_name = customer_info['name']
+print('Customer Name is Mr.',customer_name)
+print('Second purchase of Mr.', customer_name, 'is', customer_info['purchase history'][1])
+
+#Customer name and Details are registered in the Dictionary.
+#Using the Slicing Procedure, We have created a variable customer name and assigned the name from the dictionary.
+#Here, We have assigned values directly instead of using Variables.
